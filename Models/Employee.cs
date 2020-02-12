5f99fd3f96f3a64e7d6fc340b8e653163bc1f505
Stuart.Young <stuart.young@iris.co.uk>
2020-02-12 14:28:17 +0000

    Initial Employee and Location

---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+namespace FlexiTime.Models
+{
+    public class Employee
+    {
+        public int EmployeeID{get;set;}
+
+        public string Forename{get;set;}
+
+        public string Surname{get;set;}
+
+        public Location HomeOffice{get;set;}
+
+        public string Email{get;set;}
+
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,9 @@
+namespace FlexiTime.Models
+{
+    public class Location
+    {
+        public int LocationID{get;set;}
+
+        public string Name{get;set;}
+    }
+}
